0f46778ef0d11fa14d2178fc59092b466e8e346a
Ralf <ralf@xps>
2015-02-22 16:25:20 -0800

    average of each variable for each activity and each subject.

--- run_analysis.R
+++ run_analysis.R
@@ -13,14 +13,26 @@ traindf <- traindf[, cols]
 traindf$subject <- trainsub[, 1]
 # merge test and training set
 traindf <- merge(traindf, testdf, all=TRUE)
+# 
+library(plyr)
+# average of each variable for each activity and each subject.
+out <- ddply(traindf, "subject", function(x) {
+  for (i in 1:(ncol(traindf)-1)) {
+    assign(paste("c", i, sep = ""), mean(x[,i]))
+  }
+  data.frame(c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12,c13,c14,c15,c16,c17,c18,c19,c20,
+             c21,c22,c23,c24,c25,c26,c27,c28,c29,c30,c31,c32,c33,c34,c35,c36,c37,c38,
+             c39,c40,c41,c42,c43,c44,c45,c46,c47,c48,c49,c50,c51,c52,c53,c54,c55,c56,
+             c57,c58,c59,c60,c61,c62,c63,c64,c65,c66)
+})
+
 # assign descriptive names back
 features <- read.table(file="UCI HAR Dataset/features.txt")
-i=1
+i=2
 for (col in cols) {
-    colnames(traindf)[i] = as.character(features[col, 2])
+    colnames(out)[i] = as.character(features[col, 2])
     i=1+i
 }
-#
 
 #output
-write.table(traindf, file="output.txt", row.names=FALSE)
+write.table(out, file="output.txt", row.names=FALSE)
