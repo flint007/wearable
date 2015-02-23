e90cb6fc90bb54e28b19f83a010f842de3c4057a
Ralf <ralf@haiseba.com>
2015-02-23 00:42:35 -0800

    supplement activity

--- code-book.md
+++ code-book.md
@@ -2,6 +2,7 @@
 
 ## new variable for this tidy set
 "subject" - 30 participants from the original study
+"activity" - 6 activities from the original study
 
 ## inherited variables from the original study
 for this analysis, I borrowed the original variables from the features.txt and appended "-Mean" to it. It should
--- run_analysis.R
+++ run_analysis.R
@@ -1,23 +1,27 @@
 # 1 Merges the training and the test sets to create one data set.
 testdf <- read.table(file="UCI HAR Dataset/test/X_test.txt")
 testsub <- read.table(file="UCI HAR Dataset/test/subject_test.txt")
+testact <- read.table(file="UCI HAR Dataset/test/y_test.txt")
 # subsetting testset mean() and std()
 cols <- c(1,2,3,41,42,43,81,82,83,121,122,123,161,162,163,201,214,227,240,253,266,267,268,345,346,347,424,425,426,503,516,529,542,4,5,6,44,45,46,84,85,86,124,125,126,164,165,166,202,215,228,241,254,269,270,271,348,349,350,427,428,429,504,517,530,543)
 testdf <- testdf[, cols]
 testdf$subject <- testsub[, 1]
+testdf$activity <- testact[, 1]
 
 traindf <- read.table(file="UCI HAR Dataset/train/X_train.txt")
 trainsub <- read.table(file="UCI HAR Dataset/train/subject_train.txt")
+trainact <- read.table(file="UCI HAR Dataset/train/y_train.txt")
 # subsetting training set mean() and std()
 traindf <- traindf[, cols]
 traindf$subject <- trainsub[, 1]
+traindf$activity <- trainact[, 1]
 # merge test and training set
 traindf <- merge(traindf, testdf, all=TRUE)
 # 
 library(plyr)
 # average of each variable for each activity and each subject.
-out <- ddply(traindf, "subject", function(x) {
-  for (i in 1:(ncol(traindf)-1)) {
+out <- ddply(traindf, c("activity","subject"), function(x) {
+  for (i in 1:(ncol(traindf)-2)) {
     assign(paste("c", i, sep = ""), mean(x[,i]))
   }
   data.frame(c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12,c13,c14,c15,c16,c17,c18,c19,c20,
@@ -28,7 +32,7 @@ out <- ddply(traindf, "subject", function(x) {
 
 # assign descriptive names back
 features <- read.table(file="UCI HAR Dataset/features.txt")
-i=2
+i=3
 for (col in cols) {
     colnames(out)[i] = paste(as.character(features[col, 2]), "Mean",sep = "-")
     i=1+i
